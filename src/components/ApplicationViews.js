4ab2ade8f78173704788f981ee715d1f65eac476
ste163 <samtedwards@pm.me>
2020-12-31 16:19:17 -0500

    removed default react-app files and displaying Collex application views

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Redirect, Route } from "react-router-dom"
+
+export const ApplicationViews = () => {
+    return (
+        <>
+        I'm Collex!
+        </>
+    )
+}
---
+++ src/components/Collex.js
@@ -0,0 +1,7 @@
+import React from "react"
+import { Route, Redirect } from "react-router-dom"
+import { ApplicationViews } from "./ApplicationViews"
+
+export const Collex = () => (
+    <ApplicationViews />
+);
