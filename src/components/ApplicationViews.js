6c4f934d15eb3a89826cbb72baae03654514a067
ste163 <samtedwards@pm.me>
2020-12-31 22:54:58 -0500

    properly rerouting to community page

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,12 +1,21 @@
 import React from "react"
 import { Redirect, Route } from "react-router-dom"
 import { CollectionView } from "./collection/CollectionView"
+import { CommunityView } from "./community/CommunityView"
 
 export const ApplicationViews = () => {
     return (
         <>
             <Route exact path="/">
+                <Redirect to="/Collections" />
+            </Route>
+
+            <Route exact path="/Collections">
                 <CollectionView />
+            </Route>
+
+            <Route exact path="/Community">
+                <CommunityView />
             </Route>
         </>
     )
---
+++ src/components/community/CommunityView.js
@@ -0,0 +1,10 @@
+import React, { useRef, useEffect, useContext } from "react"
+
+export const CommunityView = () => {
+
+    return (
+        <>
+        <h1>Hi! I'm the Collex Community.</h1>
+        </>
+    )
+}
