debbc97c0609fc7e0f7451ec4e7c6cd2111b9320
ste163 <samtedwards@pm.me>
2021-01-01 13:30:57 -0500

    added note on how providers should most likely work

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -15,7 +15,15 @@ export const ApplicationViews = () => {
                     {/* Header only need setting and project provider */}
                     <Header />
 
-                    <Route exact path="/">
+                    {/* 
+                        Will need a Words provider, most likely, so we can compare words in our database to the thesaurus
+                        Will also need a Recents provider to show the correct recents searches for each collection
+
+                        HOWEVER, I will NOT compare Words in the Community view. I would like ot have a "Community Recents" though, that would be
+                        Just for the signed in user. So that way You could still have your recent words across public collections.
+                    */}
+
+                    <Route path="/">
                         <Redirect to="/Collections" />
                     </Route>
 
