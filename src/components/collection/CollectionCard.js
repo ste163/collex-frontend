cd0d29e79df98cdeb3c7428ed343bb260982a259
ste163 <samtedwards@pm.me>
2021-01-01 17:48:00 -0500

    styled concise collection list

--- src/components/collection/CollectionCard.js
+++ src/components/collection/CollectionCard.js
@@ -1,4 +1,5 @@
 import React, {useContext, useEffect, useRef} from "react"
+import { IconArrow } from "../icons/IconArrow"
 import "./CollectionCard.css"
 
 export const CollectionCard = ({collection}) => {
@@ -6,18 +7,17 @@ export const CollectionCard = ({collection}) => {
     // Words context??
     // const { progress, getProgressByProjectId } = useContext(ProgressContext)
 
-    console.log(collection)
-
     // useEffect(() => {
     //     getProgressByProjectId(project.id)
     // }, [])
 
     return (
         <article className="card card__color--white card__collection">
-
             <h2 className="collection__h2--card">{collection.name}</h2>
-            <p className="collection__subtitle--container">{collection.description}</p>
-
+            <p className="collection__p">{collection.description}</p>
+            <div className="collection__arrow">
+                <IconArrow color="icon__gray"/>
+            </div>
         </article>
     )
 }
