30888a0f5611e36029f378db81a642cac5186304
ste163 <samtedwards@pm.me>
2020-12-31 20:08:48 -0500

    actually added modals

---
+++ src/components/modal/Modal.js
@@ -0,0 +1,40 @@
+import React, { forwardRef } from "react"
+import { IconClose } from "../icons/IconClose"
+import "./Modal.css"
+
+// To use Modal
+    // Add props for width (see Modal.css for all):
+        // modal__width--small
+        // modal__width--wide
+    // Add content with
+        // contentFunction
+        
+export const Modal = (React.forwardRef((props, ref) => (
+     (
+        <section ref={ref} className="background__modal"
+        onClick={e => {
+            // If you click off the background, close modal
+           if (e.target.className === "background__modal modal__active") {
+            ref.current.className = "background__modal"
+           }
+        }}>
+
+            <article className={`modal__container ${props.width}`}>
+
+                <section className="modal__heading">
+
+                    <button className="modal__close"
+                    onClick={e => ref.current.className = "background__modal"}>
+                        <IconClose color="icon__gray" /></button>
+
+                </section>
+
+                <section className="modal__content">
+                    {props.contentFunction}                
+                </section>
+
+            </article>
+
+        </section>
+    )
+)))
