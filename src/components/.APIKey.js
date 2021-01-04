0c5e6e6306d48a9c1ed908f2ef3f6310906eff98
ste163 <samtedwards@pm.me>
2021-01-03 20:07:14 -0500

    added APIKey file for user to add thesaurus key

---
+++ src/components/.APIKey.js
@@ -0,0 +1,5 @@
+// To use the APIKey, make a copy of this file or rename it to APIKey.js and add your API key for
+// Merriam-Webster's Collegiate Thesaurus. The unhidden file will not be tracked by github.
+export const APIKey = {
+    thesaurus: ""
+}
