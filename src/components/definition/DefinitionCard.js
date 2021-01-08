e02f7863dcf3d7bd98e80d1000747ef6e0516835
ste163 <samtedwards@pm.me>
2021-01-07 22:09:27 -0500

    basic structure for cards

---
+++ src/components/definition/DefinitionCard.js
@@ -0,0 +1,11 @@
+import React from "react"
+// Created dynamically
+    // May need a reference to the Thesaurus column to ensure it reaches the correct location
+    // Or does each location that can create a definition card need the location (thesaurus search & word cards need the ref)?
+    // Will need the Thesaurus definition returned valued passed in as props
+
+export const DefinitionCard = () => (
+    <article>
+
+    </article>
+)
--- src/components/thesaurus/ThesaurusList.js
+++ src/components/thesaurus/ThesaurusList.js
@@ -6,11 +6,10 @@ export const ThesaurusList = () => {
 
     return (
         <section className="collection__thesaurus">
-            {/* Card labels and sizes need to be more consistent. They use too many different styles */}
             <ThesaurusSearch />
             <RecentCard />
             {/*
-                Word cards with definitions and related words. These are created dynamically.
+               Definition Cards here---created dynamically. Need ref?q
             */}
         </section>
     )
