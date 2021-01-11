ab2a0069f087bb39fb80c24e0503af7c370da810
ste163 <samtedwards@pm.me>
2021-01-10 22:27:26 -0500

    added a not found card for no definitions or possible words

--- src/components/thesaurus/ThesaurusList.js
+++ src/components/thesaurus/ThesaurusList.js
@@ -2,13 +2,13 @@ import React, { useContext } from "react"
 import { RecentCard } from "../recent/RecentCard"
 import { DefinitionCard } from "../definition/DefinitionCard"
 import { DidYouMeanCard } from "../definition/DidYouMeanCard"
+import { DefinitionNotFound } from "../definition/DefinitionNotFound"
 import { ThesaurusSearch } from "./ThesaurusSearch"
 import { ThesaurusContext } from "./ThesaurusProvider"
 import "./ThesaurusList.css"
 
 export const ThesaurusList = () => {
     const { definitionCards } = useContext(ThesaurusContext)
-    console.log("Definition cards: ", definitionCards)
 
     return (
         <section className="collection__thesaurus">
@@ -21,10 +21,12 @@ export const ThesaurusList = () => {
                     // Definition Cards created by state. Word buttons & search bar update state.
                     // Hitting the X on any card would remove it from definitionList state.
                     definitionCards.map(def => {
-                        if (def[0].meta) {
+                        if (typeof def[0] !== "string" && def[0] !== undefined) {
                             return <DefinitionCard key={definitionCards.indexOf(def)} props={def} cardId={definitionCards.indexOf(def)} />
-                        } else {
+                        } else if (def[0] !== undefined) {
                             return <DidYouMeanCard key={definitionCards.indexOf(def)} props={def} cardId={definitionCards.indexOf(def)} />
+                        } else {
+                            return <DefinitionNotFound key={definitionCards.indexOf(def)} props={def} cardId={definitionCards.indexOf(def)} />
                         }
                     })
                 }
