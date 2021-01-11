0f1a092eecf6c65ecc0ab9f0d62b96df983cd957
ste163 <samtedwards@pm.me>
2021-01-11 18:54:19 -0500

    synonyms displaying for each definition

--- src/components/definition/DefinitionCard.js
+++ src/components/definition/DefinitionCard.js
@@ -11,22 +11,12 @@ export const DefinitionCard = props => {
     // stores incoming array of all definitions for current term
     const definitions = props.props
 
-    const afterlife = {
-        word: "afterlife"
-    } 
-
-    const afternoon = {
-        word: "afternoon"
-    } 
-
-    const age = {
-        word: "age"
-    } 
-
     // DefinitionCards hold the array of current cards
     const { definitionCards, setDefinitionCards } = useContext(ThesaurusContext)
+
     // currentDef holds current definition showing in card
     const { currentDef, setCurrentDef } = useContext(DefinitionCardContext)
+
     // currently selected collection to add/remove definition from
     const { selectedCollection } = useContext(CollectionContext)
 
@@ -40,84 +30,96 @@ export const DefinitionCard = props => {
 
     return (
         !currentDef ? null : 
-        <article className="card card__color--white card__definition">
-            <button className="btn__close card__definition--close"
-            onClick={e => {
-                const removed = definitionCards.filter(card => definitionCards.indexOf(card) !== props.cardId)
-                setDefinitionCards(removed)
-            }}>
-                <IconClose color="icon__gray" />
-            </button>
+            <article className="card card__color--white card__definition">
+                <button className="btn__close card__definition--close"
+                onClick={e => {
+                    const removed = definitionCards.filter(card => definitionCards.indexOf(card) !== props.cardId)
+                    setDefinitionCards(removed)
+                }}>
+                    <IconClose color="icon__gray" />
+                </button>
 
-            <h2 className="card__h2">
-                Definition
-            </h2>
-            
-            <h3 className="card__h3 definition__h3">
-                {/* Get first item and show its name */}
-                {currentDef.meta.id}
-            </h3>
+                <h2 className="card__h2">
+                    Definition
+                </h2>
+                
+                <h3 className="card__h3 definition__h3">
+                    {/* Get first item and show its name */}
+                    {currentDef.meta.id}
+                </h3>
 
-            {
-                // If more than one definition, show next buttons to cycle through definition
-                // Which means each card needs to hold its own state, so it has the ability to change
-                // ONLY SHOW NEXT AND PREVIOUS IF THERE IS SOMETHING YOU CAN MOVE TO
-                definitions.length === 1 ? null :
-                <div className="definition__next">
-                    {
-                        definitions.indexOf(currentDef) === 0 ? null :
-                            <button 
+                {
+                    // If more than one definition, show next buttons to cycle through definition
+                    // Which means each card needs to hold its own state, so it has the ability to change
+                    // ONLY SHOW NEXT AND PREVIOUS IF THERE IS SOMETHING YOU CAN MOVE TO
+                    definitions.length === 1 ? null :
+                    <div className="definition__next">
+                        {
+                            <button
+                            disabled={definitions.indexOf(currentDef) === 0}                           
                             onClick={e => {
                                 const previous = definitions.indexOf(currentDef) - 1
                                 setCurrentDef(definitions[previous])
                             }}
                             className="btn">Previous</button>
-                    }
-                    <p className="next__text"> {definitions.indexOf(currentDef) + 1} / {definitions.length}</p>
-                    {
-                        definitions.indexOf(currentDef) === definitions.length - 1 ? null :
+                        }
+                        <p className="next__text"> {definitions.indexOf(currentDef) + 1} / {definitions.length}</p>
+                        {
                             <button
+                            disabled={definitions.indexOf(currentDef) === definitions.length - 1}  
                             onClick={e => {
                                 const next = definitions.indexOf(currentDef) + 1
                                 setCurrentDef(definitions[next])
                             }}
                             className="btn">Next</button>
+                        }
+                    </div>
+                }
+
+                <h4 className="card__h4 definition__h4--speech">
+                    {currentDef.fl}
+                </h4>
+                <div className="card__definition--text">
+                    {
+                        // RENAME THIS CONTAINER
+                        // NEED TO IMPROVE STYLING
+                        currentDef.shortdef.map(shortDefinition => {
+                            return <p key={currentDef.shortdef.indexOf(shortDefinition)}>{shortDefinition}</p>
+                        })
                     }
                 </div>
-            }
-
-            <h4 className="card__h4 definition__h4--speech">
-                noun
-            </h4>
-            <div className="card__definition--text">
+                
                 {
-                    // RENAME THIS CONTAINER
-                    // NEED TO IMPROVE STYLING
-                    currentDef.shortdef.map(shortDefinition => {
-                        return <p key={currentDef.shortdef.indexOf(shortDefinition)}>{shortDefinition}</p>
-                    })
+                    // If there are no synonyms, don't show the section
+                    currentDef.meta.syns.length === 0 ? null :
+                        <>
+                            <h4 className="card__h4 definition__h4--synonym">
+                                synonyms
+                            </h4>
+
+                            {/* word button list */}
+                            <ul className="word__list definition__words">
+                                {
+                                    currentDef.meta.syns.map(synonymArray => {
+                                        return synonymArray.map(synonym => {
+                                            return <WordButton key={synonymArray.indexOf(synonym)} props={{word: synonym}} />
+                                        })
+                                    })
+                                }
+                            </ul>
+                        </>
+
                 }
-            </div>
-            <h4 className="card__h4 definition__h4--synonym">
-                synonyms
-            </h4>
+                
 
-            {/* word button list */}
-            <ul className="word__list definition__words">
-                <WordButton key={1} props={afterlife} />
-                <WordButton key={2} props={afternoon} />
-                <WordButton key={3} props={age} />
-            </ul>
-            {/* stems array of: 'autumn, autumnal, autumnally, autumns, fall' */}
-
-            {/* IF word is already in the user's collection, change this to REMOVE */}
-            {
-                selectedCollection.id === 0 ? null :
-                <button className="btn definition__submit"
-                onClick={e => console.log("ADD ME TO COLLECTION")}>
-                    Add to {selectedCollection.name}
-                </button>
-            }
-        </article>
+                {/* IF word is already in the user's collection, change this to REMOVE */}
+                {
+                    selectedCollection.id === 0 ? null :
+                    <button className="btn definition__submit"
+                    onClick={e => console.log("ADD ME TO COLLECTION")}>
+                        Add to {selectedCollection.name}
+                    </button>
+                }
+            </article>
     )
 }
