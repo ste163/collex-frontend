48479a218ade4dc9b52cf9d21e676d0b0167c5c7
ste163 <samtedwards@pm.me>
2021-01-24 17:50:25 -0500

    showing synonyms for definitions

--- src/views/main/thesaurus/definition/DefinitionCardSynonyms.js
+++ src/views/main/thesaurus/definition/DefinitionCardSynonyms.js
@@ -1,5 +1,8 @@
 // Render synonyms for current definition
+// Also sorts synonyms into arrays of 10
 import React, { useEffect, useState } from "react"
+import { ChangeIconClassOnHover } from "../../../../utils/ChangeIconClassOnHover"
+import { IconArrow } from "../../../../components/icons/Icons"
 import WordButton from "../../../../components/word/WordButton"
 
 const DefinitionCardSynonyms = ({ currentDef }) => {
@@ -7,10 +10,40 @@ const DefinitionCardSynonyms = ({ currentDef }) => {
     // Final Array of Arrays of all Synonyms separated into sets of 10
     const [ arrayOfSynonymArrays, setArrayOfSynonymArrays ] = useState([])
 
+    // setState for previous and next buttons
+    const [ synPrevBtnDisabled, setSynPrevBtnDisabled ] = useState(true)
+    const [ synNextBtnDisabled, setSynNextBtnDisabled ] = useState(false)
+
+    // Current synonymArray from array of arrays
+    const [ currentSynArray, setCurrentSynArray ] = useState([])
+
+    // Separated so we can get the current array of arrays of synonyms
     useEffect(() => {
         setArrayOfSynonymArrays(SeparateListOfSynsIntoTens())
-        console.log("Array of Arrays", arrayOfSynonymArrays)
     }, [currentDef])
+
+    // Whenever the arrayOfSynonymsArrays state changes, update state of CurrentSynArray
+    useEffect(() => {
+        setCurrentSynArray(arrayOfSynonymArrays[0])
+        console.log("First Array", arrayOfSynonymArrays[0] )
+        console.log("Array of Arrays", arrayOfSynonymArrays)
+    }, [arrayOfSynonymArrays])
+
+    // Whenever the CurrentSynArray state changes, update Next & Prev button States
+    useEffect(() => {
+        // PreviousDefinitionBtn State
+        if (arrayOfSynonymArrays.indexOf(currentSynArray) === 0) {
+            setSynPrevBtnDisabled(true)
+        } else {
+            setSynPrevBtnDisabled(false)
+        }
+        // NextDefinitionBtn State
+        if (arrayOfSynonymArrays.indexOf(currentSynArray) === arrayOfSynonymArrays.length - 1) {
+            setSynNextBtnDisabled(true)
+        } else {
+            setSynNextBtnDisabled(false)
+        }
+    }, [currentSynArray])
     
     // Find total of synonyms for "Showing X out of TOTAL"
     const CalculateTotalSyns = () => {
@@ -24,6 +57,10 @@ const DefinitionCardSynonyms = ({ currentDef }) => {
     // Create a combined list of all synonyms for this definition
     // Returns nothing because it's affecting allSynonyms
     const CreateArrayOfAllSyns = () => {
+        if (currentDef.meta.syns.length === 0) {
+            return null
+        }
+
         let allSynonyms = []
         currentDef.meta.syns.forEach(synonymArray => {
             synonymArray.forEach(synonym => {
@@ -59,24 +96,53 @@ const DefinitionCardSynonyms = ({ currentDef }) => {
         return null
     }
 
+    if (currentSynArray === undefined) {
+        return null
+    }
+
     return (
         <>
             <hr className="definition__divider"></hr>
-            <h4 className="card__h4 definition__h4--synonym">
-                synonyms
-            </h4>
+            <h4 className="card__h4 definition__h4--synonym">synonyms</h4>
             {
                 arrayOfSynonymArrays.length === 1 ? null :
                 // Only show if there is more than one "page" of synonyms
-                <p className="synonym__total">
-                    Showing X out of {CalculateTotalSyns()}
-                </p>
+                <>
+                <p className="synonym__total">Showing {currentSynArray.length} out of {CalculateTotalSyns()}</p>
+                
+                {/* MAKE THIS A COMPONENT BECAUSE IT'S USED BY BOTH DEFINITIONS AND SYNONYMS */}
+                <div className="synonym__next">
+                <button                      
+                    onClick={e => {
+                        const previous = arrayOfSynonymArrays.indexOf(currentSynArray) - 1
+                        setCurrentSynArray(arrayOfSynonymArrays[previous])
+                    }}
+                    onMouseOver={e => ChangeIconClassOnHover(e, true, "icon__black", "icon__white")}
+                    onMouseLeave={e => ChangeIconClassOnHover(e, false, "icon__black", "icon__white")}
+                    className={arrayOfSynonymArrays.indexOf(currentSynArray) === 0 ? "btn btn__arrow btn__disabled" : "btn btn__arrow"}>
+                        <IconArrow rotation="icon__arrow--rotated" color="icon__black" disabled={synPrevBtnDisabled} />
+                    </button>
+
+                    <p className="next__text"> {arrayOfSynonymArrays.indexOf(currentSynArray) + 1} / {arrayOfSynonymArrays.length}</p>
+                    
+                    <button
+                    onClick={e => {
+                        const next = arrayOfSynonymArrays.indexOf(currentSynArray) + 1
+                        setCurrentSynArray(arrayOfSynonymArrays[next])
+                    }}
+                    onMouseOver={e => ChangeIconClassOnHover(e, true, "icon__black", "icon__white")}
+                    onMouseLeave={e => ChangeIconClassOnHover(e, false, "icon__black", "icon__white")}
+                    className={arrayOfSynonymArrays.indexOf(currentSynArray) === arrayOfSynonymArrays.length - 1 ? "btn btn__arrow btn__disabled" : "btn btn__arrow"}>
+                        <IconArrow color="icon__black" disabled={synNextBtnDisabled} />
+                    </button>
+                </div>
+                </>
             }
-            <ul className="word__list definition__words">
+            <ul className="word__list synonym__words">
                 {
-                    // allSynonyms.map(synonym => {
-                    //     return <WordButton key={allSynonyms.indexOf(synonym)} props={{word: synonym}} />
-                    // })
+                    currentSynArray.map(synonym => {
+                        return <WordButton key={currentSynArray.indexOf(synonym)} props={{word: synonym}} />
+                    })
                 }
             </ul>
         </>
